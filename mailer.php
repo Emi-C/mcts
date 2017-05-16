9ec749050a5b5b4bba6144139b408679e150536a
Emi <e.costanzo@lswr.it>
2017-05-16 23:39:57 +0200

    press mockup, contatti

--- mailer.php
+++ mailer.php
@@ -2,7 +2,7 @@
 $mail=$_POST["mail"];
 $msg=$_POST["msg"];
 
-$headers = 'From: wwe.it <no-reply@wwe.it>';
+$headers = "From: Michetta's <no-reply@michettas.it>"";
 echo $headers;
 $sent=mail( 'emil.costanzo@gmail.com', 'Nuovo messaggio da '.$name.' '.$cog.' - '.$mail, $msg, $headers);
 if ( !$sent ) {   http_response_code(400);} else {   http_response_code(200);}?>
